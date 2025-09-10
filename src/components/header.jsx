4f5b84dca272ab3f98fa17babb05fc03d916db43
almohanad khalil <almohanedkhalil@gmail.com>
2025-09-10 15:42:31 +0300

    first init

---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div className="navbar">
+      <div className="navbar-logo">WEATHER</div>
+      <div className="searchbar">
+        <input type="text" className="search" placeholder="Search..."/>
+        <button className="search-submit">ss</button>
+      </div>
+    </div>
+  );
+}
