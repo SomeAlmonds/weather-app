87e610cc32cd2dc9211f1ef4b7163a93a976099b
almohanad khalil <almohanedkhalil@gmail.com>
2025-09-17 22:04:49 +0300

    finished extracting and organizing forecast data not including the styling

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -4,7 +4,7 @@ const { night, clouds, lightClouds } = images;
 
 // TODO: integrate map API
 export function Body({ forecastObj }) {
-  const { current, location, forecast} = forecastObj;
+  const { current, location, forecast } = forecastObj;
 
   return (
     <div className="body">
@@ -18,7 +18,7 @@ export function Body({ forecastObj }) {
       </div>
 
       {/* ////////////////////////////////////// */}
-      
+
       <div className="body-content">
         <div className="primary-info">
           <div className="p-info1">
@@ -40,7 +40,7 @@ export function Body({ forecastObj }) {
               <p>UV INDEX: {uvIndexScale(current?.uv)}</p>
             </div>
           </div>
-          
+
           <div className="p-info2">
             <div>
               <p>{location?.name}</p>
@@ -48,26 +48,72 @@ export function Body({ forecastObj }) {
               <p>{location?.country}</p>
             </div>
             <icons.locationIcon />
-          </div>      
+          </div>
         </div>
         {/* //////////////////////////////////////////////// */}
         <div className="forcast-info">
-          <ForecastDays forecast={forecast}/>
+          <ForecastDays forecast={forecast} />
         </div>
       </div>
     </div>
   );
 }
 
-
-function ForecastDays({forecast}){
+function ForecastDays({ forecast }) {
   // logic for upcoming days forcast component
 
-  return (forecast?.forecastday.map((day) => {
-    <div className="day">
-      
-    </div>
-  }))
+  // NOTE:
+  // WeatherAPI for some reason returns the current day with the forecast object for the next days
+  // and the free plan only offers 3 days but showing only tow for the forcast object doesn't look nice
+  // so I'm including the current day in the forecast section aswell
+
+  return forecast?.forecastday.map((day) => {
+    let date = new Date(day.date);
+    let weekday = String(date).split(" ")[0];
+
+    return (
+      <div className="day" key={date}>
+        <p>{weekday}</p>
+        <div className="sun-rise-set">
+          <div>
+            <icons.SunRise /> {day.astro.sunrise}
+          </div>
+          <div>
+            <icons.SunSet /> {day.astro.sunset}
+          </div>
+        </div>
+        <div className="avg-temp">
+          {`Min: ${day.day.mintemp_c} | Max: ${day.day.maxtemp_c}`}
+        </div>
+
+        <div id={"expand-" + weekday} style={{ display: "none" }}>
+          <div className="day-condition">
+            {day.day.condition.text}{" "}
+            <img
+              src={day.day.condition.icon}
+              alt={`${day.day.condition.text} icon`}
+              width={40}
+            />
+          </div>
+
+          <p>Chance of rain: {day.day.daily_chance_of_rain}%</p>
+          <p>Max wind speed: {day.day.maxwind_kph}</p>
+          <p>UV index: {uvIndexScale(day.day.uv)}</p>
+        </div>
+
+        <button
+          type="button"
+          className="expand-info-btn"
+          onClick={() => {
+            let element = document.getElementById("expand-" + weekday);
+            element.getAttribute("style", "display")
+              ? element.removeAttribute("style")
+              : element.setAttribute("style", "display: none");
+          }}
+        ><icons.ChevronIcon /></button>
+      </div>
+    );
+  });
 }
 
 function uvIndexScale(uvNum) {
